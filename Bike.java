c48945622d4d789400531769f20e606f41e9a243
selvask12 <selvaamech17@gmail.com>
2022-09-03 11:06:43 +0530

    test

---
+++ Bike.java
@@ -0,0 +1,12 @@
+package Test1;
+
+public class Bike {
+	public static void main(String[] args) {
+		String nums[]=args[0].split(",");
+		String value=nums[0];
+		String value1=nums[1];
+		System.out.println(value1.toUpperCase());
+		
+	}
+
+}
